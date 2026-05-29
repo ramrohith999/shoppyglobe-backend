4cdd565e21e0db3babf90dc9f78b4839cd586f48
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-29 15:57:34 +0530

    created prduct model

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    stockQuantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Product",
+  productSchema
+);
